c847711f249d6d8116d8d2e8b7e4f5cb57fb7b20
athenaz14 <athenaz11@nycstudents.net>
2026-03-17 09:26:15 -0400

    update

---
+++ Dictionary_Project.py
@@ -0,0 +1,45 @@
+items = [
+{
+    "name": "Pencil",
+    "price": 2.00,
+    "department": "Stationery",
+    "description": "Writing/Drawing instrument with graphite."
+    
+},
+{
+    "name": "Eraser",
+    "price": 1.00,
+    "department": "Stationery",
+    "description": "A tool to remove graphite."
+},
+{
+"name": "Paper",
+    "price": 2.00,
+    "department": "Stationery",
+    "description": "150 sheets of printer paper."
+},
+{
+"name": "Marker",
+    "price": 5.00,
+    "department": "Stationery",
+    "description": "Felt tip pen."
+
+},
+{
+"name": "Pen",
+    "price": 1.50,
+    "department": "Stationery",
+    "description": "Writing/Drawing Instrument with ink."
+},
+{
+"name": "Looseleaf",
+    "price": 1.50,
+    "department": "Stationery",
+    "description": "A sheet with three holes punched"
+}
+]
+
+
+print(items[0]["name"])
+for index, item in enumerate(items):
+    print(index, ":", item["name"])
